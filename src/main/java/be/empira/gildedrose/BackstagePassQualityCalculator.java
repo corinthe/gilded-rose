a4ca1522879caa01a4baa739be8c1a0f9c87d426
Antoine Rassart <antoinerassart@hotmail.be>
2019-10-27 18:44:15 +0100

    Refactored increaseQuality logic to follow english explanation more closely

--- src/main/java/be/empira/gildedrose/BackstagePassQualityCalculator.java
+++ src/main/java/be/empira/gildedrose/BackstagePassQualityCalculator.java
@@ -1,5 +1,7 @@
 package be.empira.gildedrose;
 
+import static java.lang.Math.min;
+
 public class BackstagePassQualityCalculator extends QualityCalculator {
 
     public BackstagePassQualityCalculator(Item item) {
@@ -13,21 +15,12 @@ public class BackstagePassQualityCalculator extends QualityCalculator {
 
     @Override
     void increaseQuality() {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-
+        if (item.sellIn <= 5) {
+            item.quality = min(50, item.quality + 3);
+        } else if (item.sellIn <= 10) {
+            item.quality = min(50, item.quality + 2);
+        } else {
+            item.quality += 1;
         }
     }
 
