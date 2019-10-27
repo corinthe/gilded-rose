95678710304360241092a16a7344f10480344ef0
Antoine Rassart <antoinerassart@hotmail.be>
2019-10-27 18:21:19 +0100

    Added a calculator for Aged Brie items

---
+++ src/main/java/be/empira/gildedrose/AgedBrieQualityCalculator.java
@@ -0,0 +1,8 @@
+package be.empira.gildedrose;
+
+public class AgedBrieQualityCalculator extends QualityCalculator {
+
+    public AgedBrieQualityCalculator(Item item) {
+        super(item);
+    }
+}
--- src/main/java/be/empira/gildedrose/QualityCalculatorFactory.java
+++ src/main/java/be/empira/gildedrose/QualityCalculatorFactory.java
@@ -3,6 +3,9 @@ package be.empira.gildedrose;
 public class QualityCalculatorFactory {
 
     public static QualityCalculator create(Item item) {
-        return new QualityCalculator(item);
+        switch (item.name) {
+            case "Aged Brie": return new AgedBrieQualityCalculator(item);
+            default: return new QualityCalculator(item);
+        }
     }
 }
