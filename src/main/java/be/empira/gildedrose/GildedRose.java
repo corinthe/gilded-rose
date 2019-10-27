6e4cb99b3fe5ab04cf0f73b6f6b370d8ec5b4020
Antoine Rassart <antoinerassart@hotmail.be>
2019-10-27 17:38:25 +0100

    Extracted method isBackstagePass

--- src/main/java/be/empira/gildedrose/GildedRose.java
+++ src/main/java/be/empira/gildedrose/GildedRose.java
@@ -10,7 +10,7 @@ public class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!isAgedBrie(items[i])
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    && !isBackstagePass(items[i])) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
@@ -20,7 +20,7 @@ public class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isBackstagePass(items[i])) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -42,7 +42,7 @@ public class GildedRose {
 
             if (items[i].sellIn < 0) {
                 if (!isAgedBrie(items[i])) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (!isBackstagePass(items[i])) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
@@ -62,5 +62,9 @@ public class GildedRose {
 
     private boolean isAgedBrie(Item item) {
         return item.name.equals("Aged Brie");
+    }
+
+    private boolean isBackstagePass(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 }
